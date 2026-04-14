3c0628d254b7606a2754d6ab74011366be6554e7
wangxiaoyun <wangxiaoyun@dailyinnovation.biz>
2026-04-14 11:48:28 +0800

    fix: AppStartTaskLogUtil 日志改用 Log.i()，删除无用 import

--- appstartfasterlibrary/src/main/java/com/aice/appstartfaster/util/AppStartTaskLogUtil.java
+++ appstartfasterlibrary/src/main/java/com/aice/appstartfaster/util/AppStartTaskLogUtil.java
@@ -2,14 +2,12 @@ package com.aice.appstartfaster.util;
 
 import android.util.Log;
 
-import com.aice.appstartfaster.dispatcher.AppStartTaskDispatcher;
-
 public class AppStartTaskLogUtil {
     private static final String TAG = "AppStartTask ";
 
     public static void showLog(boolean isShowLog, String log) {
         if (isShowLog) {
-            Log.e(TAG, log);
+            Log.i(TAG, log);
         }
     }
 }
